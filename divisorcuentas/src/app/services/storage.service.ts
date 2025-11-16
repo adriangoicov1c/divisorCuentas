47b921b3ca80e1d93f7bf9365332fb329563f4a7
adrian <adrian.goicovic@gmail.com>
2025-11-16 08:01:30 -0300

    Implementando iconos y splash

--- divisorcuentas/src/app/services/storage.service.ts
+++ divisorcuentas/src/app/services/storage.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 
+@Injectable({
+  providedIn: 'root'
+})
+
 export class StorageService {
 
   private _storage: Storage | null = null;
